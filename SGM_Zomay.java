bb8d441275dcfbf559cbcfea57765a930c5a7260
zedektrillo <zedektrillo@gmail.com>
2026-04-14 23:06:01 -0600

    Add files via upload

---
+++ SGM_Zomay.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.zomay.sgm;
+
+/**
+ *
+ * @author zedek
+ */
+public class SGM_Zomay {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
